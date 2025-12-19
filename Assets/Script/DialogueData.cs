097bd9809fd2bf5033d65c3c75a3e780890bb690
HyeonjungYun <guswnd9971@gmail.com>
2025-12-20 00:08:47 +0900

    fix : merge bug fix

--- Assets/Script/DialogueData.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "New Dialogue", menuName = "Dialogue System/Dialogue")]
-public class DialogueData : ScriptableObject
-{
-    public string speakerName;
-    [TextArea(3, 10)]
-    public string[] sentences; // 대사 내용들
-}
